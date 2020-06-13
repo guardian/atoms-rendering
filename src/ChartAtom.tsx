3450dfa4b49a17b03a9c94d2a1daa8d786bc8e2f
Pascal Honoré <pascal@alseyn.net>
2020-06-14 00:33:50 +0100

    Correct atom type

--- src/ChartAtom.tsx
+++ src/ChartAtom.tsx
@@ -8,7 +8,7 @@ import { ChartAtomType } from './types';
 export const ChartAtom = ({ id, url }: ChartAtomType): JSX.Element => (
     <div
         data-atom-id={id}
-        data-atom-type="interactive"
+        data-atom-type="chart"
         className={css`
             padding-bottom: ${space[1]}px;
             padding-left: ${space[2]}px;
