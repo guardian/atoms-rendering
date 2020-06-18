d2270a0ea7c99d3456e779a0bd99c19c4a436fce
William Lacroix <william.lacroix.extern@joyn.de>
2020-06-18 11:39:58 +0100

    apply styles only to iframe

--- src/InteractiveAtom.tsx
+++ src/InteractiveAtom.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 
 import { space } from '@guardian/src-foundations';
 import { text } from '@guardian/src-foundations/palette';
@@ -10,20 +10,22 @@ const fullWidthStyles = css`
     width: 100%;
 `;
 
+const iframeStyles = css`
+    padding-bottom: ${space[1]}px;
+    padding-left: ${space[2]}px;
+    padding-right: ${space[2]}px;
+    color: ${text.primary};
+`;
+
 export const InteractiveAtom = ({
     id,
     url,
 }: InteractiveAtomType): JSX.Element => (
-    <div
-        data-atom-id={id}
-        data-atom-type="interactive"
-        className={css`
-            padding-bottom: ${space[1]}px;
-            padding-left: ${space[2]}px;
-            padding-right: ${space[2]}px;
-            color: ${text.primary};
-        `}
-    >
-        <iframe className={fullWidthStyles} src={url} frameBorder="0"></iframe>
+    <div data-atom-id={id} data-atom-type="interactive">
+        <iframe
+            className={cx(fullWidthStyles, iframeStyles)}
+            src={url}
+            frameBorder="0"
+        />
     </div>
 );
