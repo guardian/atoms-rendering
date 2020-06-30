2e70b8247a63856168d035344059dbbc91b02c8d
William Lacroix <william.lacroix.extern@joyn.de>
2020-06-30 14:52:59 +0200

    add fonts using JS

--- src/lib/unifyPageContent.tsx
+++ src/lib/unifyPageContent.tsx
@@ -29,12 +29,25 @@ export const unifyPageContent = ({
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
-                        function resize() {
-                            window.frameElement.height = document.body.offsetHeight;
-                        }
-                        window.addEventListener('resize', resize);
-                        resize();
-                    `,
+                            function resize() {
+                                window.frameElement.height = document.body.offsetHeight;
+                            }
+                            window.addEventListener('resize', resize);
+                            resize();
+                        `,
+                    }}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            var fonts = [].slice.apply(window.parent.document.styleSheets)
+                                .filter(function (sheet) { return sheet.ownerNode.className.indexOf("webfont") > - 1; })
+                                .map(function (sheet) { return sheet.ownerNode.textContent; })
+                                .join(' ');
+                            var css = document.createElement('style');
+                            css.textContent = fonts;
+                            document.head.appendChild(css);
+                        `,
                     }}
                 />
             </html>
