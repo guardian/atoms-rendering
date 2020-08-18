c7d9d3dc2bf8df88b02371812756e925e9b56f50
Mark Addis <mark.addis@guardian.co.uk>
2020-08-18 12:36:23 +0100

    Refactored Guide and QandA atoms

--- src/GuideAtom.test.tsx
+++ src/GuideAtom.test.tsx
@@ -3,27 +3,12 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 
 import { GuideAtom } from './GuideAtom';
+import { defaultStory } from '../fixtures/guideAtom';
 
 describe('GuideAtom', () => {
     it('should render', () => {
         const { getByText, queryByText } = render(
-            <GuideAtom
-                id="a76d998e-d4b0-4d00-8afb-773eddb4064c"
-                label="Quick Guide"
-                title="Wednesday's Hong Kong tips"
-                html="<p><b>Happy Valley&nbsp;</b></p><p><b>11.45</b> Happy Good Guys <b>12.15</b> Salto Olimpico <b>12.45</b> Seize The Spirit <b>1.15</b> Allied Agility <b>1.45 </b>Hero Time <b>2.15</b> Simply Fluke <b>2.45</b> Brave King <b>3.15</b> Golden Dash <b>3.50</b> This Is Class</p>"
-                credit=""
-                pillar=""
-                likeHandler={() => {
-                    return null;
-                }}
-                dislikeHandler={() => {
-                    return null;
-                }}
-                expandCallback={() => {
-                    return null;
-                }}
-            />,
+            <GuideAtom {...defaultStory} />,
         );
 
         expect(getByText('Quick Guide')).toBeInTheDocument();
@@ -40,23 +25,7 @@ describe('GuideAtom', () => {
 
     it('Show feedback on like', () => {
         const { getByText, queryByText, queryByTestId } = render(
-            <GuideAtom
-                id="a76d998e-d4b0-4d00-8afb-773eddb4064c"
-                label="Quick Guide"
-                title="Wednesday's Hong Kong tips"
-                html="<p><b>Happy Valley&nbsp;</b></p><p><b>11.45</b> Happy Good Guys <b>12.15</b> Salto Olimpico <b>12.45</b> Seize The Spirit <b>1.15</b> Allied Agility <b>1.45 </b>Hero Time <b>2.15</b> Simply Fluke <b>2.45</b> Brave King <b>3.15</b> Golden Dash <b>3.50</b> This Is Class</p>"
-                credit=""
-                pillar=""
-                likeHandler={() => {
-                    return null;
-                }}
-                dislikeHandler={() => {
-                    return null;
-                }}
-                expandCallback={() => {
-                    return null;
-                }}
-            />,
+            <GuideAtom {...defaultStory} />,
         );
 
         // Expand Guide
@@ -74,23 +43,7 @@ describe('GuideAtom', () => {
 
     it('Show feedback on dislike', () => {
         const { getByText, queryByText, queryByTestId } = render(
-            <GuideAtom
-                id="a76d998e-d4b0-4d00-8afb-773eddb4064c"
-                label="Quick Guide"
-                title="Wednesday's Hong Kong tips"
-                html="<p><b>Happy Valley&nbsp;</b></p><p><b>11.45</b> Happy Good Guys <b>12.15</b> Salto Olimpico <b>12.45</b> Seize The Spirit <b>1.15</b> Allied Agility <b>1.45 </b>Hero Time <b>2.15</b> Simply Fluke <b>2.45</b> Brave King <b>3.15</b> Golden Dash <b>3.50</b> This Is Class</p>"
-                credit=""
-                pillar=""
-                likeHandler={() => {
-                    return null;
-                }}
-                dislikeHandler={() => {
-                    return null;
-                }}
-                expandCallback={() => {
-                    return null;
-                }}
-            />,
+            <GuideAtom {...defaultStory} />,
         );
 
         // Expand Guide
