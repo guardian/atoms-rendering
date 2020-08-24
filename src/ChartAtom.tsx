ac020dfd7ef2ffc908ce1fd4b4cb238d23101bd5
Mark Addis <mark.addis@guardian.co.uk>
2020-08-24 16:47:21 +0100

    Based on Pascals work

--- src/ChartAtom.tsx
+++ src/ChartAtom.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { css } from 'emotion';
 import { space } from '@guardian/src-foundations';
-import { neutral, text } from '@guardian/src-foundations/palette';
+import { text } from '@guardian/src-foundations/palette';
 
 import { ChartAtomType } from './types';
 
@@ -13,11 +13,9 @@ export const ChartAtom = ({ id, url }: ChartAtomType): JSX.Element => (
             padding-bottom: ${space[1]}px;
             padding-left: ${space[2]}px;
             padding-right: ${space[2]}px;
-            border-top: 1px solid ${text.primary};
             color: ${text.primary};
-            background: ${neutral[97]};
         `}
     >
-        <iframe src={url} frameBorder="0" height="450px"></iframe>
+        <iframe src={url} frameBorder="0" width="100%" height="540px"></iframe>
     </div>
 );
