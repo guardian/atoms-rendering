953ad9404c6652f13d11cdf184ae0eb4ec973826
Mark Addis <mark.addis@guardian.co.uk>
2020-08-24 16:49:05 +0100

    update test

--- src/ChartAtom.test.tsx
+++ src/ChartAtom.test.tsx
@@ -6,7 +6,12 @@ import { ChartAtom } from './ChartAtom';
 
 describe('ChartAtom', () => {
     it('should render', () => {
-        const { getByText } = render(<ChartAtom id="123abc" />);
+        const { getByText } = render(
+            <ChartAtom
+                url="https://api.nextgen.guardianapps.co.uk/embed/atom/chart/16597f9d-cc9c-4659-bfcb-1a1fcbbf6263"
+                id="123abc"
+            />,
+        );
 
         // expect(getByText('ChartAtom')).toBeInTheDocument();
     });
