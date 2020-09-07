8d0e54264d56a83322088586d8396c64729f6451
Gareth <gtrufitt@gmail.com>
2020-09-07 15:17:12 +0100

    Add scripts for resizing and swapping the gu-script
    These scripts are likely to come from the platform, but we want to use them in our story here, so that we're able to run the scripts and resize the atom as expected

--- src/ChartAtom.stories.tsx
+++ src/ChartAtom.stories.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { ChartAtom } from './ChartAtom';
 import { html } from '../fixtures/chartAtoms';
-import { css } from 'emotion';
+
+import { atomResizer, atomGuScriptSwap } from './lib/platformExampleScripts';
+
 export default {
     title: 'ChartAtom',
     component: ChartAtom,
 };
 
-const js =
-    "<script>var iframes = document.getElementsByClassName('atom__iframe');console.log(iframes);window.addEventListener('message', (event) => {const iframe = iframes.find((i) => {try {return i.name === event.source.name;} catch (e) {return false;}});if (iframe) {try {const message = JSON.parse(event.data);switch (message.type) {case 'set-height':iframe.height = message.value;break;default:}} catch (e) {}}});iframes.forEach((iframe) => {const src = (iframe.getAttribute('srcdoc') || '').replace(/<gu-script>/g, '<script>').replace(/</gu-script>/g, '<' + '/script>');iframe.setAttribute('srcdoc', src);});</script>";
-
 export const DefaultStory = (): JSX.Element => {
-    //console.log(html);
-    //const html2 = html.replace('</body>', js + '</body>');
-
+    useEffect(() => {
+        atomResizer();
+        atomGuScriptSwap();
+    }, []);
     return (
         <ChartAtom
             id="abc123"
