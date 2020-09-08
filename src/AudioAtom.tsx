646d174d8431b250f985cffcc5af3a27ecb8b09e
Pascal Honoré <pascal@alseyn.net>
2020-09-08 19:23:23 +0100

    -

--- src/AudioAtom.tsx
+++ src/AudioAtom.tsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 import { AudioAtomType } from './types';
 
-export const AudioAtom = ({ id }: AudioAtomType): JSX.Element => (
-    <div data-atom-id={id}>AudioAtom</div>
-);
+export const AudioAtom = ({
+    trackUrl,
+    kicker,
+    title,
+}: AudioAtomType): JSX.Element => {
+    return (
+        <figure>
+            <figcaption>{kicker}</figcaption>
+            {title && <p>{title}</p>}
+            <audio controls src={trackUrl}>
+                <p>
+                    {' '}
+                    Sorry your browser does not support audio - but you can
+                    download here and listen
+                    https://audio.guim.co.uk/2020/05/05-61553-gnl.fw.200505.jf.ch7DW.mp3{' '}
+                </p>
+            </audio>
+        </figure>
+    );
+};
--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,9 @@ export type AdTargeting = {
 };
 
 export type AudioAtomType = {
-    id: string;
+    trackUrl: string;
+    kicker: string;
+    title?: string;
 };
 
 export type ChartAtomType = {
