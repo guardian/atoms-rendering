162228381303e83d24c26f3a2ef52f6666295530
Amy Hughes <amy.hughes@guardian.co.uk>
2020-09-18 10:24:38 +0100

    remove nested functions as className s, use cx instead. Remove anonymous functions from inline styles

--- src/QuizAtom.tsx
+++ src/QuizAtom.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, KeyboardEvent } from 'react';
 import { SvgCheckmark, SvgCross } from '@guardian/src-icons';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 import { neutral, news } from '@guardian/src-foundations/palette';
 import { body, textSans } from '@guardian/src-foundations/typography';
 import { visuallyHidden } from '@guardian/src-foundations/accessibility';
@@ -12,39 +12,21 @@ import {
     Answer as AnswerType,
 } from './types';
 
-const enum AnswerState {
-    CorrectChosen,
-    CorrectUnchosen,
-    IncorrectChosen,
-    IncorrectUnchosen,
-    Unanswered,
-}
-
-type AnswerStateArgs = {
-    isAnswered: boolean;
-    isChosen: boolean;
-    isCorrect: boolean;
-};
-
-const answerState = ({ isAnswered, isChosen, isCorrect }: AnswerStateArgs) => {
-    if (!isAnswered) {
-        return AnswerState.Unanswered;
-    } else if (isCorrect && isChosen) {
-        return AnswerState.CorrectChosen;
-    } else if (isCorrect && !isChosen) {
-        return AnswerState.CorrectUnchosen;
-    } else if (!isCorrect && isChosen) {
-        return AnswerState.IncorrectChosen;
-    } else {
-        return AnswerState.IncorrectUnchosen;
-    }
-};
-
-const neutralBackgroundColour = neutral[97];
-const darkerNeutralBackgroundColour = neutral[86];
-const correctBackgroundColour = 'rgb(61, 181, 64)';
-const correctUnchosenBackgroundColour = 'rgb(61, 181, 64, 0.6)';
-const incorrectBackgroundColour = news[400];
+const neutralBackgroundColour = css`
+    background-color: ${neutral[97]};
+`;
+const darkerNeutralBackgroundColour = css`
+    background-color: ${neutral[86]};
+`;
+const opaqueGreenBackgroundColour = css`
+    background-color: rgb(61, 181, 64);
+`;
+const translucentGreenBackgroundColour = css`
+    background-color: rgb(61, 181, 64, 0.6);
+`;
+const redBackgroundColour = css`
+    background-color: ${news[400]};
+`;
 
 const commonLabelStyle = css`
     svg {
@@ -56,56 +38,17 @@ const commonLabelStyle = css`
     transition: background-colour 100ms;
 `;
 
-const unansweredLabel = css`
+const focusLabelStyle = css`
     cursor: pointer;
-    background-color: ${neutralBackgroundColour};
-    ${commonLabelStyle}
-    background-color: ${neutralBackgroundColour};
+    ${neutralBackgroundColour};
     :hover {
-        background-color: ${darkerNeutralBackgroundColour};
+        ${darkerNeutralBackgroundColour};
     }
     :focus {
         ${focusHalo}
-        background-color: ${darkerNeutralBackgroundColour};
+        ${darkerNeutralBackgroundColour};
     }
 `;
-
-const correctChosenLabel = css`
-    background-color: ${correctBackgroundColour};
-    ${commonLabelStyle}
-`;
-
-const correctUnchosenLabel = css`
-    background-color: ${correctUnchosenBackgroundColour};
-    ${commonLabelStyle}
-`;
-
-const incorrectChosenLabel = css`
-    background-color: ${incorrectBackgroundColour};
-    ${commonLabelStyle}
-`;
-
-const incorrectUnchosenLabel = css`
-    background-color: ${neutralBackgroundColour};
-    ${commonLabelStyle}
-`;
-
-const labelStyle = (state: AnswerState) => {
-    switch (state) {
-        case AnswerState.Unanswered:
-            return unansweredLabel;
-        case AnswerState.CorrectChosen:
-            return correctChosenLabel;
-        case AnswerState.CorrectUnchosen:
-            return correctUnchosenLabel;
-        case AnswerState.IncorrectChosen:
-            return incorrectChosenLabel;
-        case AnswerState.IncorrectUnchosen:
-            return incorrectUnchosenLabel;
-        default:
-            return commonLabelStyle;
-    }
-};
 
 const commonTextStyle = css`
     padding-left: 8px;
@@ -113,29 +56,13 @@ const commonTextStyle = css`
     min-height: auto;
 `;
 
-const unchosenText = css`
+const blackColour = css`
     color: ${neutral[7]};
-    ${commonTextStyle};
 `;
-
-const chosenText = css`
+const whiteColour = css`
     color: ${neutral[100]};
-    ${commonTextStyle};
 `;
-
-const textStyle = (state: AnswerState) => {
-    switch (state) {
-        case AnswerState.IncorrectChosen:
-            return chosenText;
-        case AnswerState.CorrectChosen:
-            return chosenText;
-        default:
-            return unchosenText;
-    }
-};
-
-const revealTextStyle = (state: AnswerState) => css`
-    ${textStyle(state)}
+const revealTextStyle = css`
     ${textSans.xsmall()}
 `;
 
@@ -264,67 +191,78 @@ export const Answer = ({
     isChosen,
     setChosen,
     isAnswered,
-}: AnswerType & AnswerProps): JSX.Element => (
-    <div
-        className={css`
-            clear: both;
-            margin-bottom: 6px;
-            position: relative;
-        `}
-    >
-        <input
-            type="radio"
-            tabIndex={-1}
-            id={`answer-${id}`}
-            required
-            defaultChecked={isChosen}
-            onClick={() => setChosen(id)}
-            disabled={isAnswered}
+}: AnswerType & AnswerProps): JSX.Element => {
+    const handleClick = () => setChosen(id);
+    const handleKeyPress = (e: KeyboardEvent) => {
+        if (e.key === 'Enter' && !isAnswered) {
+            setChosen(id);
+        }
+    };
+    return (
+        <div
             className={css`
-                ${visuallyHidden};
+                clear: both;
+                margin-bottom: 6px;
+                position: relative;
             `}
-        ></input>
-        <label
-            htmlFor={`answer-${id}`}
-            tabIndex={0}
-            className={labelStyle(
-                answerState({ isAnswered, isChosen, isCorrect }),
-            )}
-            onKeyPress={(e) => {
-                if (e.key === 'Enter' && !isAnswered) {
-                    setChosen(id);
-                }
-            }}
         >
-            <div className={answerStyle}>
-                {isAnswered &&
-                    isChosen &&
-                    (isCorrect
-                        ? IconWrapper(<SvgCheckmark />)
-                        : IconWrapper(<SvgCross />))}
-                <div>
-                    <span
-                        className={textStyle(
-                            answerState({ isAnswered, isChosen, isCorrect }),
-                        )}
-                    >
-                        {text}
-                    </span>
-                    {isAnswered && revealText && (
+            <input
+                type="radio"
+                tabIndex={-1}
+                id={`answer-${id}`}
+                required
+                defaultChecked={isChosen}
+                onClick={handleClick}
+                disabled={isAnswered}
+                className={css`
+                    ${visuallyHidden};
+                `}
+            ></input>
+            <label
+                htmlFor={`answer-${id}`}
+                tabIndex={0}
+                className={cx({
+                    [commonLabelStyle]: true,
+                    [focusLabelStyle]: !isAnswered,
+                    [opaqueGreenBackgroundColour]: isCorrect && isChosen,
+                    [translucentGreenBackgroundColour]:
+                        isCorrect && !isChosen && isAnswered,
+                    [redBackgroundColour]: !isCorrect && isChosen,
+                    [neutralBackgroundColour]: !isCorrect && !isChosen,
+                })}
+                onKeyPress={handleKeyPress}
+            >
+                <div className={answerStyle}>
+                    {isAnswered &&
+                        isChosen &&
+                        (isCorrect
+                            ? IconWrapper(<SvgCheckmark />)
+                            : IconWrapper(<SvgCross />))}
+                    <div>
                         <span
-                            className={revealTextStyle(
-                                answerState({
-                                    isAnswered,
-                                    isChosen,
-                                    isCorrect,
-                                }),
-                            )}
+                            className={cx({
+                                [commonTextStyle]: true,
+                                [whiteColour]: isChosen,
+                                [blackColour]: !isChosen,
+                            })}
                         >
-                            {revealText}
+                            {text}
                         </span>
-                    )}
+                        {isAnswered && revealText && (
+                            <span
+                                className={cx({
+                                    [commonTextStyle]: true,
+                                    [revealTextStyle]: true,
+                                    [whiteColour]: isChosen,
+                                    [blackColour]: !isChosen,
+                                })}
+                            >
+                                {revealText}
+                            </span>
+                        )}
+                    </div>
                 </div>
-            </div>
-        </label>
-    </div>
-);
+            </label>
+        </div>
+    );
+};
