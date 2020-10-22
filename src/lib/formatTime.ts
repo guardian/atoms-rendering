070c6a13fb8b3517803ee4bf9de3dba757dc366d
William Lacroix <william.lacroix.extern@joyn.de>
2020-10-22 16:45:27 +0100

    test format time method

--- src/lib/formatTime.ts
+++ src/lib/formatTime.ts
@@ -1,5 +1,13 @@
+const enforceTwoDigitString = (number: number) =>
+    number >= 10 ? number : `0${number}`;
+
 export const formatTime = (videoDurationInSeconds: number) => {
-    const minutes = Math.floor(videoDurationInSeconds / 60);
+    const hours = Math.floor(videoDurationInSeconds / 3600);
+    const minutes = Math.floor((videoDurationInSeconds % 3600) / 60);
     const seconds = videoDurationInSeconds % 60;
-    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+    const hoursString = hours > 0 ? `${enforceTwoDigitString(hours)}:` : '';
+    const minutesString = minutes > 0 ? enforceTwoDigitString(minutes) : '00';
+    const secondsString = seconds > 0 ? enforceTwoDigitString(seconds) : '00';
+    return `${hoursString}${minutesString}:${secondsString}`;
 };
