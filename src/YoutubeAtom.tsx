ce4a0849c8938371c60b10ffa3dfcac053ad8d7f
William Lacroix <william.j.t.lacroix@gmail.com>
2020-11-17 17:44:23 +0000

    Add analytics for youtube video

--- src/YoutubeAtom.tsx
+++ src/YoutubeAtom.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { css } from 'emotion';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import { MaintainAspectRatio } from './common/MaintainAspectRatio';
-
 import { YoutubeOverlay } from './YoutubeOverlay';
+
+declare let window: any;
 
 type EmbedConfig = {
     adsConfig: {
@@ -55,6 +55,8 @@ type YoutubeMeta = {
     width?: string;
 };
 
+type VideoEventKey = 'play' | '25' | '50' | '75' | 'end' | 'skip';
+
 type YoutubeAtomType = {
     videoMeta: YoutubeMeta;
     overlayImage?: string;
@@ -64,7 +66,13 @@ type YoutubeAtomType = {
     title?: string;
     duration?: number; // in seconds
     origin?: string;
+    gaEventEmitter?: (event: VideoEventKey) => void;
+    ophanEventEmitter?: (event: VideoEventKey) => void;
 };
+
+type YoutubeStateChangeEvent = { data: -1 | 0 | 1 | 2 | 3 | 5 };
+
+let progressTracker: NodeJS.Timeout | null;
 
 // Note, this is a subset of the CAPI MediaAtom essentially.
 export const YoutubeAtom = ({
@@ -76,11 +84,175 @@ export const YoutubeAtom = ({
     title = 'YouTube video player',
     duration,
     origin,
+    gaEventEmitter,
+    ophanEventEmitter,
 }: YoutubeAtomType): JSX.Element => {
     const embedConfig =
         adTargeting && JSON.stringify(buildEmbedConfig(adTargeting));
     const originString = origin ? `&origin=${origin}` : '';
     const iframeSrc = `https://www.youtube.com/embed/${videoMeta.assetId}?embed_config=${embedConfig}&enablejsapi=1${originString}&widgetid=1&modestbranding=1`;
+
+    const [hasUserClickedPlay, setHasUserClickedPlay] = useState<boolean>(
+        false,
+    );
+    const [isPlayerReady, setIsPlayerReady] = useState<boolean>(false);
+    const [player, setPlayer] = useState<YT.Player | null>(null);
+
+    const onPlayerStateChange = (e: YoutubeStateChangeEvent) => {
+        /** YouTube API
+            -1 (unstarted)
+            0 (ended)
+            1 (playing)
+            2 (paused)
+            3 (buffering)
+            5 (video cued)
+        **/
+        switch (e.data) {
+            // playing
+            case 1: {
+                // need to make sure user is not resuming
+                if (!hasUserClickedPlay) {
+                    gaEventEmitter && gaEventEmitter('play');
+                    ophanEventEmitter && ophanEventEmitter('play');
+                }
+                setHasUserClickedPlay(true);
+
+                progressTracker = setInterval(() => {
+                    if (!player) {
+                        // player is not yet defined
+                        // eslint-disable-next-line no-console
+                        console.error(`Youtube player object not available`);
+                        return;
+                    }
+
+                    if (!gaEventEmitter)
+                        // eslint-disable-next-line no-console
+                        console.error(`GA function is not available`);
+                    if (!ophanEventEmitter)
+                        // eslint-disable-next-line no-console
+                        console.error(`Ophan function is not available`);
+
+                    const currentTime = player.getCurrentTime();
+                    const duration = player.getDuration();
+                    const percentPlayed = Math.round(
+                        (currentTime / duration) * 100,
+                    );
+
+                    switch (percentPlayed) {
+                        case 25: {
+                            gaEventEmitter && gaEventEmitter('25');
+                            ophanEventEmitter && ophanEventEmitter('25');
+                            break;
+                        }
+                        case 50: {
+                            gaEventEmitter && gaEventEmitter('50');
+                            ophanEventEmitter && ophanEventEmitter('50');
+                            break;
+                        }
+                        case 75: {
+                            gaEventEmitter && gaEventEmitter('75');
+                            ophanEventEmitter && ophanEventEmitter('75');
+                            break;
+                        }
+                    }
+                }, 1000);
+                break;
+            }
+            // paused
+            case 2: {
+                progressTracker && clearInterval(progressTracker);
+                break;
+            }
+            // ended
+            case 0: {
+                gaEventEmitter && gaEventEmitter('end');
+                ophanEventEmitter && ophanEventEmitter('end');
+                progressTracker && clearInterval(progressTracker);
+            }
+        }
+    };
+
+    const loadVideo = () => {
+        setPlayer(
+            new window.YT.Player(`${videoMeta.assetId}`, {
+                videoId: `${videoMeta.assetId}`,
+                events: {
+                    onReady: () => setIsPlayerReady(true),
+                    // Issue with setting events on Youtube object (refer to useEffect below)
+                    // https://stackoverflow.com/a/17078152
+                    // onPlayerStateChange,
+                },
+                playerVars: {
+                    mute: 1,
+                    autoplay: 1,
+                },
+            }),
+        );
+    };
+
+    useEffect(() => {
+        if (player) {
+            // Issue with setting events on Youtube object
+            // https://stackoverflow.com/a/17078152
+
+            // ts-ignore is used because onStateChange has different listener to what is actually sent
+            // @ts-ignore
+            player.addEventListener('onStateChange', onPlayerStateChange);
+        }
+    }, [player]);
+
+    useEffect(() => {
+        // if window is undefined it is because this logic is running on the server side
+        if (typeof window === 'undefined') return;
+
+        if (window.YT) {
+            loadVideo();
+        } else {
+            // If not, load the script asynchronously
+            const tag = document.createElement('script');
+            tag.src = 'https://www.youtube.com/iframe_api';
+
+            // onYouTubeIframeAPIReady will load the video after the script is loaded
+            window.onYouTubeIframeAPIReady = loadVideo;
+
+            const firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag &&
+                firstScriptTag.parentNode &&
+                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        }
+    }, []);
+
+    const onClickOverlay = useCallback(() => {
+        if (isPlayerReady && player) {
+            try {
+                player.playVideo();
+            } catch (e) {
+                // eslint-disable-next-line no-console
+                console.error(`Unable to play video due to: ${e}`);
+            }
+        }
+    }, [player, isPlayerReady]);
+
+    const onKeyDownOverlay = useCallback(
+        (e) => {
+            const spaceKey = 32;
+            const enterKey = 13;
+            if (
+                (e.keyCode === spaceKey || e.keyCode === enterKey) &&
+                isPlayerReady &&
+                player
+            ) {
+                try {
+                    player.playVideo();
+                } catch (e) {
+                    // eslint-disable-next-line no-console
+                    console.error(`Unable to play video due to: ${e}`);
+                }
+            }
+        },
+        [player, isPlayerReady],
+    );
+
     return (
         <MaintainAspectRatio height={height} width={width}>
             <iframe
@@ -98,7 +270,9 @@ export const YoutubeAtom = ({
                 <YoutubeOverlay
                     image={overlayImage}
                     duration={duration}
-                    id={videoMeta.assetId}
+                    hideOverlay={hasUserClickedPlay}
+                    onClickOverlay={onClickOverlay}
+                    onKeyDownOverlay={onKeyDownOverlay}
                 />
             )}
         </MaintainAspectRatio>
--- src/YoutubeOverlay.tsx
+++ src/YoutubeOverlay.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React from 'react';
 import { css } from 'emotion';
 
 import { palette, space } from '@guardian/src-foundations';
@@ -6,8 +6,6 @@ import { textSans } from '@guardian/src-foundations/typography';
 import { SvgPlay } from '@guardian/src-icons';
 
 import { formatTime } from './lib/formatTime';
-
-declare let window: any;
 
 const overlayStyles = (image: string) => css`
     background-image: url(${image});
@@ -76,102 +74,34 @@ const videoDurationStyles = css`
 export const YoutubeOverlay = ({
     image,
     duration,
-    id,
+    hideOverlay,
+    onClickOverlay,
+    onKeyDownOverlay,
 }: {
     image: string;
     duration?: number;
-    id: string;
-}): JSX.Element => {
-    const [hideOverlay, setHideOverlay] = useState(false);
-    const [isPlayerReady, setIsPlayerReady] = useState(false);
-    const [player, setPlayer] = useState<YT.Player | null>(null);
-
-    const loadVideo = () => {
-        setPlayer(
-            new window.YT.Player(`${id}`, {
-                events: {
-                    onReady: () => setIsPlayerReady(true),
-                },
-                playerVars: {
-                    mute: 1,
-                    autoplay: 1,
-                },
-            }),
-        );
-    };
-
-    useEffect(() => {
-        // if window is undefined it is because this logic is running on the server side
-        if (typeof window === 'undefined') return;
-
-        if (window.YT) {
-            loadVideo();
-        } else {
-            // If not, load the script asynchronously
-            const tag = document.createElement('script');
-            tag.src = 'https://www.youtube.com/iframe_api';
-
-            // onYouTubeIframeAPIReady will load the video after the script is loaded
-            window.onYouTubeIframeAPIReady = loadVideo;
-
-            const firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag &&
-                firstScriptTag.parentNode &&
-                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        }
-    }, []);
-
-    const onClickOverlay = useCallback(() => {
-        if (isPlayerReady && player) {
-            try {
-                player.playVideo();
-                setHideOverlay(true);
-            } catch (e) {
-                console.error(`Unable to play video due to: ${e}`);
-            }
-        }
-    }, [player, isPlayerReady]);
-
-    const onKeyDownOverlay = useCallback(
-        (e) => {
-            const spaceKey = 32;
-            const enterKey = 13;
-            if (
-                (e.keyCode === spaceKey || e.keyCode === enterKey) &&
-                isPlayerReady &&
-                player
-            ) {
-                try {
-                    player.playVideo();
-                    setHideOverlay(true);
-                } catch (e) {
-                    console.error(`Unable to play video due to: ${e}`);
-                }
-            }
-        },
-        [player, isPlayerReady],
-    );
-
-    return (
-        <div
-            // cannot use cx because it would cause new key to be generated on `hideOverlay` toggle causing css refresh
-            className={`${overlayStyles(image)} ${
-                hideOverlay ? hideOverlayStyling : ''
-            }`}
-            onClick={onClickOverlay}
-            onKeyDown={onKeyDownOverlay}
-            tabIndex={0}
-        >
-            <div className={overlayInfoWrapperStyles}>
-                <div className={`${playButtonStyling} overlay-play-button`}>
-                    <SvgPlay />
-                </div>
-                {duration && (
-                    <div className={videoDurationStyles}>
-                        {formatTime(duration)}
-                    </div>
-                )}
+    hideOverlay: boolean;
+    onClickOverlay: () => void;
+    onKeyDownOverlay: (e: React.KeyboardEvent<HTMLDivElement>) => void;
+}): JSX.Element => (
+    <div
+        // cannot use cx because it would cause new key to be generated on `hideOverlay` toggle causing css refresh
+        className={`${overlayStyles(image)} ${
+            hideOverlay ? hideOverlayStyling : ''
+        }`}
+        onClick={onClickOverlay}
+        onKeyDown={onKeyDownOverlay}
+        tabIndex={0}
+    >
+        <div className={overlayInfoWrapperStyles}>
+            <div className={`${playButtonStyling} overlay-play-button`}>
+                <SvgPlay />
             </div>
+            {duration && (
+                <div className={videoDurationStyles}>
+                    {formatTime(duration)}
+                </div>
+            )}
         </div>
-    );
-};
+    </div>
+);
