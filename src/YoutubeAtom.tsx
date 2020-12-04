011971fe65749b752e2f1c9d827c29c380bacdd7
William Lacroix <william.j.t.lacroix@gmail.com>
2020-12-04 10:30:53 +0000

    fix linting issues

--- src/YoutubeAtom.tsx
+++ src/YoutubeAtom.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { css, cx } from 'emotion';
 import YouTubePlayer from 'youtube-player';
 
