a398cdaa79f69d1ed39fd7013ac03984aeb85df6
William Lacroix <william.j.t.lacroix@gmail.com>
2021-02-03 14:46:20 +0000

    update babel configs

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
 module.exports = {
     presets: [
         '@babel/preset-typescript',
-        ['@babel/preset-react', { pragma: 'h', pragmaFrag: 'Fragment' }],
-        '@emotion/babel-preset-css-prop',
+        [
+            '@babel/preset-react',
+            {
+                runtime: 'automatic',
+                importSource: '@emotion/core',
+            },
+        ],
         [
             '@babel/preset-env',
             {
@@ -11,6 +16,7 @@ module.exports = {
                 },
             },
         ],
+        '@emotion/babel-preset-css-prop',
     ],
     plugins: ['@babel/plugin-proposal-optional-chaining'],
     env: {
