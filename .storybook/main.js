2231fe9c1a9f6d5b9cc588e371e24bf1b237328d
Fares Marek Basmadji <fares.basmadji@guardian.co.uk>
2021-03-16 12:51:27 +0000

    reconfigure storybook loaders

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,21 +3,7 @@ module.exports = {
     webpackFinal: async (config) => {
         config.module.rules.push({
             test: /\.(ts|tsx)$/,
-            loader: require.resolve('babel-loader'),
-            options: {
-                presets: [
-                    '@babel/preset-typescript',
-                    '@emotion/babel-preset-css-prop',
-                    [
-                        '@babel/preset-env',
-                        {
-                            targets: {
-                                esmodules: true,
-                            },
-                        },
-                    ],
-                ],
-            },
+            loader: require.resolve('ts-loader'),
         });
 
         return config;
