73b671694d6307ec7067b25abe59f86a9265909a
William Lacroix <william.j.t.lacroix@gmail.com>
2021-05-27 14:28:45 +0100

    fix linting issues

--- src/PersonalityQuiz.stories.tsx
+++ src/PersonalityQuiz.stories.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import { css } from '@emotion/react';
->>>>>>> 889024b17ace5fc90be2648daab6cd100dd29507
 
 import { PersonalityQuizAtom } from './PersonalityQuiz';
 import {
@@ -16,7 +12,7 @@ export default {
     component: PersonalityQuizAtom,
 };
 
-export const DefaultRendering = () => (
+export const DefaultRendering = (): JSX.Element => (
     <PersonalityQuizAtom
         id="quiz-id"
         questions={examplePersonalityQuestions}
