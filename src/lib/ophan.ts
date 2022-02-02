eca2a82376e8f58838d8e44b63f5ad6dad3af2e9
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2022-02-02 14:06:36 +0000

    docs: Added a comment explaining the source for this code

--- src/lib/ophan.ts
+++ src/lib/ophan.ts
@@ -1,5 +1,14 @@
 import type { OphanComponentEvent } from '@guardian/libs';
 
+/**
+ * This code is duplicated from DCR. This is intentional. We do not have an established
+ * centralised solution for this type of code (making tracking requests) but it is
+ * an area we are thinking about. In the meantime, we are keeping use cases separate
+ * so that it will be easier to make any transition to a centralised solution
+ * later
+ *
+ * See: https://github.com/guardian/dotcom-rendering/blob/88a31f1d74d2fb09d051ff75b513b4cbf31fdd23/dotcom-rendering/src/web/browser/ophan/ophan.ts
+ */
 declare global {
     /* ~ Here, declare things that go in the global namespace, or augment
      *~ existing declarations in the global namespace
