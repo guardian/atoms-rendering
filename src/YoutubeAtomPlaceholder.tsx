8c2ecb64767548889cd900ef11db700d9a9eb57e
Ravi Lal <arelra@users.noreply.github.com>
2022-02-18 16:31:11 +0000

    YoutubeAtomPlaceholder fade in

--- src/YoutubeAtomPlaceholder.tsx
+++ src/YoutubeAtomPlaceholder.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
+
+const fadeInOpacity = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const transitionOpacityIn = css`
+    animation: ${fadeInOpacity} 500ms ease-in 0s 1;
+`;
 
 export const YoutubeAtomPlaceholder = (): JSX.Element => {
     return (
         <div
             data-name="youtube-placeholder"
             data-testid="youtube-placeholder"
-            css={css`
-                width: 100%;
-                height: 100%;
-                position: absolute;
-                top: 0;
-                left: 0;
-                display: flex;
-                flex-grow: 1;
-                background-color: black;
-            `}
+            css={[
+                css`
+                    width: 100%;
+                    height: 100%;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    display: flex;
+                    flex-grow: 1;
+                    background-color: black;
+                    ${transitionOpacityIn}
+                `,
+            ]}
         ></div>
     );
 };
