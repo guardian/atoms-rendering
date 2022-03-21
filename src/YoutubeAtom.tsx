d769575f9dd0c78fb73210baa9abd0d41c8edb85
Joe Cowton <joe.cowton@guardian.co.uk>
2022-03-21 16:55:53 +0000

    Create YoutubeAtomSticky component

--- src/YoutubeAtom.tsx
+++ src/YoutubeAtom.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { MaintainAspectRatio } from './common/MaintainAspectRatio';
 import { YoutubeAtomPlayer } from './YoutubeAtomPlayer';
 import { YoutubeAtomOverlay } from './YoutubeAtomOverlay';
 import { YoutubeAtomPlaceholder } from './YoutubeAtomPlaceholder';
+import { YoutubeAtomSticky } from './YoutubeAtomSticky';
 import type {
     AdTargeting,
     ImageSource,
@@ -10,12 +11,7 @@ import type {
     VideoEventKey,
 } from './types';
 import type { ArticleTheme } from '@guardian/libs';
-import { log } from '@guardian/libs';
 import type { ConsentState } from '@guardian/consent-management-platform/dist/types';
-import { useIsInView } from './lib/useIsInView';
-import { from, neutral } from '@guardian/source-foundations';
-import { css } from '@emotion/react';
-import { SvgCross } from '@guardian/source-react-components';
 
 type Props = {
     assetId: string;
@@ -34,90 +30,6 @@ type Props = {
     pillar: ArticleTheme;
     shouldStick: boolean;
 };
-
-const buttonStyles = css`
-    position: absolute;
-    left: -36px;
-    top: 0;
-    z-index: 22;
-    background-color: ${neutral[7]};
-    height: 32px;
-    width: 32px;
-    border-radius: 50%;
-    border: 0;
-    padding: 0;
-    cursor: pointer;
-    display: none;
-    justify-content: center;
-    align-items: center;
-    transition: transform 0.2s;
-
-    &:hover {
-        transform: scale(1.15);
-    }
-
-    svg {
-        fill: ${neutral[100]};
-    }
-`;
-
-/**
- * This extended hover area allows users to click the close video button more easily
- */
-const hoverAreaStyles = css`
-    position: absolute;
-    top: -4px;
-    bottom: 0;
-    left: -${32 * 1.15}px;
-    width: ${32 * 1.15}px;
-
-    &:hover button {
-        display: flex;
-    }
-`;
-
-const stickyStyles = css`
-    @keyframes fade-in-up {
-        from {
-            transform: translateY(100%);
-            opacity: 0;
-        }
-
-        to {
-            transform: translateY(0%);
-            opacity: 1;
-        }
-    }
-
-    position: fixed;
-    bottom: 20px;
-    width: 216px;
-    z-index: 21;
-    animation: fade-in-up 1s ease both;
-
-    &:hover button {
-        display: flex;
-    }
-
-    ${from.tablet} {
-        width: 300px;
-    }
-
-    figcaption {
-        display: none;
-    }
-`;
-
-const stickyContainerStyles = (height: number) => css`
-    height: ${height}px;
-    position: relative;
-    display: flex;
-    justify-content: flex-end;
-
-    ${from.tablet} {
-        height: ${height * 2}px;
-    }
-`;
 
 export const YoutubeAtom = ({
     assetId,
@@ -139,15 +51,7 @@ export const YoutubeAtom = ({
     const [overlayClicked, setOverlayClicked] = useState<boolean>(false);
     const [playerReady, setPlayerReady] = useState<boolean>(false);
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
-    const [isSticky, setIsSticky] = useState<boolean>(false);
-    const [stickEventSent, setStickEventSent] = useState<boolean>(false);
     const [stopVideo, setStopVideo] = useState<boolean>(false);
-
-    const [isIntersecting, setRef] = useIsInView({
-        threshold: 0.5,
-        repeat: true,
-        debounce: true,
-    });
 
     /**
      * Update the isPlaying state based on video events
@@ -171,48 +75,6 @@ export const YoutubeAtom = ({
      * Combine the videoState and tracking event emitters
      */
     const compositeEventEmitters = [playerState, ...eventEmitters];
-
-    /**
-     * Click handler for the sticky video close button
-     */
-    const handleCloseClick = () => {
-        // unstick the video
-        setIsSticky(false);
-        // reset the sticky event sender
-        setStickEventSent(false);
-        // stop the video
-        setStopVideo(true);
-        // emit a 'close' event
-        log('dotcom', {
-            from: `YoutubeAtom handleCloseClick`,
-            videoId: assetId,
-            msg: 'Close',
-        });
-        eventEmitters.forEach((eventEmitter) => eventEmitter('close'));
-    };
-
-    /**
-     * useEffect for the sticky state
-     */
-    useEffect(() => {
-        if (shouldStick) setIsSticky(isPlaying && !isIntersecting);
-    }, [isIntersecting, isPlaying, shouldStick]);
-
-    /**
-     * useEffect for the stick events
-     */
-    useEffect(() => {
-        if (isSticky && !stickEventSent) {
-            setStickEventSent(true);
-
-            log('dotcom', {
-                from: 'YoutubeAtom stick useEffect',
-                videoId: assetId,
-                msg: 'Stick',
-            });
-            eventEmitters.forEach((eventEmitter) => eventEmitter('stick'));
-        }
-    }, [isSticky, stickEventSent, assetId, eventEmitters]);
 
     const hasOverlay = !!(overrideImage || posterImage);
 
@@ -255,57 +117,53 @@ export const YoutubeAtom = ({
     const playerReadyCallback = useCallback(() => setPlayerReady(true), []);
 
     return (
-        <div ref={setRef} css={isSticky && stickyContainerStyles(192)}>
-            <div css={isSticky && stickyStyles}>
-                {isSticky && (
-                    <>
-                        <span css={hoverAreaStyles} />
-                        <button css={buttonStyles} onClick={handleCloseClick}>
-                            <SvgCross size="medium" />
-                        </button>
-                    </>
+        <YoutubeAtomSticky
+            assetId={assetId}
+            shouldStick={shouldStick}
+            isPlaying={isPlaying}
+            eventEmitters={eventEmitters}
+            onStopVideo={() => setStopVideo(true)}
+        >
+            <MaintainAspectRatio height={height} width={width}>
+                {loadPlayer && consentState && (
+                    <YoutubeAtomPlayer
+                        videoId={assetId}
+                        overrideImage={overrideImage}
+                        posterImage={posterImage}
+                        adTargeting={adTargeting}
+                        consentState={consentState}
+                        height={height}
+                        width={width}
+                        title={title}
+                        origin={origin}
+                        eventEmitters={compositeEventEmitters}
+                        /**
+                         * If there is an overlay we want to autoplay
+                         * If there is not an overlay the user will use the YouTube player UI to play
+                         */
+                        autoPlay={hasOverlay}
+                        onReady={playerReadyCallback}
+                        stopVideo={stopVideo}
+                    />
                 )}
-                <MaintainAspectRatio height={height} width={width}>
-                    {loadPlayer && consentState && (
-                        <YoutubeAtomPlayer
-                            videoId={assetId}
-                            overrideImage={overrideImage}
-                            posterImage={posterImage}
-                            adTargeting={adTargeting}
-                            consentState={consentState}
-                            height={height}
-                            width={width}
-                            title={title}
-                            origin={origin}
-                            eventEmitters={compositeEventEmitters}
-                            /**
-                             * If there is an overlay we want to autoplay
-                             * If there is not an overlay the user will use the YouTube player UI to play
-                             */
-                            autoPlay={hasOverlay}
-                            onReady={playerReadyCallback}
-                            stopVideo={stopVideo}
-                        />
-                    )}
-                    {showOverlay && (
-                        <YoutubeAtomOverlay
-                            videoId={assetId}
-                            overrideImage={overrideImage}
-                            posterImage={posterImage}
-                            height={height}
-                            width={width}
-                            alt={alt}
-                            role={role}
-                            duration={duration}
-                            pillar={pillar}
-                            onClick={() => setOverlayClicked(true)}
-                        />
-                    )}
-                    {showPlaceholder && (
-                        <YoutubeAtomPlaceholder videoId={assetId} />
-                    )}
-                </MaintainAspectRatio>
-            </div>
-        </div>
+                {showOverlay && (
+                    <YoutubeAtomOverlay
+                        videoId={assetId}
+                        overrideImage={overrideImage}
+                        posterImage={posterImage}
+                        height={height}
+                        width={width}
+                        alt={alt}
+                        role={role}
+                        duration={duration}
+                        pillar={pillar}
+                        onClick={() => setOverlayClicked(true)}
+                    />
+                )}
+                {showPlaceholder && (
+                    <YoutubeAtomPlaceholder videoId={assetId} />
+                )}
+            </MaintainAspectRatio>
+        </YoutubeAtomSticky>
     );
 };
