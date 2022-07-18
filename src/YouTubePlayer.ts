ede6aa978b72364b4d276640ec8e162c72c286ed
Ravi <7014230+arelra@users.noreply.github.com>
2022-07-18 23:15:09 +0100

    Restructure YouTubePlayer internal promises

--- src/YouTubePlayer.ts
+++ src/YouTubePlayer.ts
@@ -7,42 +7,32 @@ type EmbedConfig = {
         adsConfig: AdsConfig;
     };
 };
+
 type PlayerOptions = YT.PlayerOptions & EmbedConfig;
 
 class YouTubePlayer {
-    id: string;
-    playerOptions: PlayerOptions;
-    player: YT.Player | undefined;
-    playerInitialized?: Promise<YT.Player>;
+    playerPromise: Promise<YT.Player>;
 
     constructor(id: string, playerOptions: PlayerOptions) {
-        this.id = id;
-        this.playerOptions = playerOptions;
-        this.setup();
+        this.playerPromise = this.setPlayer(id, playerOptions);
     }
 
-    private async setup() {
-        await loadYouTubeAPI();
-        return this.getOrInitializePlayer(this.id, this.playerOptions);
-    }
-
-    private getOrInitializePlayer(id: string, playerOptions: PlayerOptions) {
-        if (this.playerInitialized) {
-            return this.playerInitialized;
-        }
-        this.playerInitialized = new Promise<YT.Player>((resolve) => {
-            this.player = new YT.Player(id, playerOptions);
-            resolve(this.player);
+    private async setPlayer(id: string, playerOptions: PlayerOptions) {
+        const YTAPI = await loadYouTubeAPI();
+        const playerPromise = new Promise<YT.Player>((resolve) => {
+            const player = new YTAPI.Player(id, playerOptions);
+            resolve(player);
         });
-        return this.playerInitialized;
+        return playerPromise;
     }
 
     async getPlayerState() {
-        const player = await this.setup();
+        const player = await this.playerPromise;
         return player.getPlayerState();
     }
+
     async stopVideo() {
-        const player = await this.setup();
+        const player = await this.playerPromise;
         player.stopVideo();
     }
 }
