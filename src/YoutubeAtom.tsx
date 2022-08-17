78b921342f9763c93f076ecdf1376ba26aff4ad6
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-08-17 16:02:12 +0100

    remove comment

--- src/YoutubeAtom.tsx
+++ src/YoutubeAtom.tsx
@@ -134,7 +134,6 @@ export const YoutubeAtom = ({
             isMainMedia={isMainMedia}
         >
             <MaintainAspectRatio height={height} width={width}>
-                {/* TODO maintain aspect ratio's padding is messing up ad container styles */}
                 {loadPlayer && consentState && (
                     <YoutubeAtomPlayer
                         videoId={videoId}
