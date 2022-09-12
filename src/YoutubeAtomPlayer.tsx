2cc23e51757d116353ee0042ca6d7ee39bc2e9d8
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-09-12 14:56:18 +0100

    Update YoutubeAtomPlayer.tsx

--- src/YoutubeAtomPlayer.tsx
+++ src/YoutubeAtomPlayer.tsx
@@ -30,6 +30,10 @@ declare class ImaManager {
 
 declare global {
     interface Window {
+        /**
+         * Here we want to type the google object that will be added to window.
+         * Since the imported google namespace is a value rather than a type, use typeof.
+         */
         google: typeof google;
         YT: {
             ImaManager: typeof ImaManager;
