d148b6140e004466b63531ee528442cc580a8bbf
Anna Beddow <anna.beddow@gmail.com>
2022-09-22 15:13:59 +0100

    Refactor: remove and condense styles

--- src/YoutubeAtomCaption.tsx
+++ src/YoutubeAtomCaption.tsx
@@ -1,63 +1,12 @@
 import { css } from '@emotion/react';
-import { space, textSans } from '@guardian/source-foundations';
-import React, { useState } from 'react';
+import { space, textSans, neutral } from '@guardian/source-foundations';
+import { SvgVideo } from '@guardian/source-react-components';
+import { useState } from 'react';
 
-const captionLink = css`
-    a {
-        color: white;
-        text-decoration: none;
-    }
-    a:hover {
-        text-decoration: underline;
-    }
-    strong {
-        font-weight: bold;
-    }
-`;
-
-const videoRatio = (23 / 36) * 100;
-
-const iconStyle = css`
-    fill: white;
-    margin-right: ${space[1]}px;
-    display: inline-block;
-    position: relative;
-    width: 1.1em;
-    vertical-align: baseline;
-    ::before {
-        content: ' ';
-        display: block;
-        padding-top: ${videoRatio}%;
-    }
-    svg {
-        top: 0px;
-        right: 0px;
-        bottom: 0px;
-        left: 0px;
-        width: 100%;
-        position: absolute;
-        height: 100%;
-    }
-`;
-
-const captionStyle = css`
-    color: white;
-    ${textSans.xxsmall()};
-    padding: 6px 40px 0px 12px;
-    word-wrap: break-all;
-    left: 0;
-    right: 0;
-    top: 0;
-    background: rgba(18, 18, 18, 0.8);
-    flex-grow: 1;
-    min-height: 42px;
-    position: absolute;
-    z-index: 1;
-`;
 const buttonStyle = css`
     position: absolute;
     top: 6px;
-    right: 5px;
+    right: 6px;
     width: 32px;
     height: 32px;
     z-index: 2;
@@ -73,7 +22,50 @@ const buttonStyle = css`
         left: 0;
         margin: auto;
         position: absolute;
-        fill: white;
+        fill: ${neutral[100]};
+    }
+`;
+
+const figCaptionStyle = css`
+    position: absolute;
+    z-index: 1;
+    display: flex;
+    flex-grow: 1;
+    min-height: 44px;
+    padding: 6px 40px 0px 12px;
+    background: rgba(18, 18, 18, 0.8);
+    color: ${neutral[100]};
+    ${textSans.xxsmall()};
+    word-wrap: break-all;
+    top: 0;
+    left: 0;
+    right: 0;
+`;
+
+const iconStyle = css`
+    fill: ${neutral[100]};
+    margin-right: ${space[1]}px;
+`;
+const textStyle = css`
+    margin-top: 1px;
+`;
+
+const captionStyle = css`
+    a {
+        color: ${neutral[100]};
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+    strong {
+        font-weight: bold;
+    }
+`;
+const creditStyle = css`
+    ::before {
+        content: ' ';
+        white-space: pre;
     }
 `;
 type Props = {
@@ -98,21 +90,19 @@ export const YoutubeAtomCaption = ({ caption, credit }: Props): JSX.Element => {
                     <path d="M4.6 12l-.4 1.4c-.7.2-1.9.6-3 .6-.7 0-1.2-.2-1.2-.9 0-.2 0-.3.1-.5l2-6.7H.7l.4-1.5 4.2-.6h.2L3 12h1.6zm-.3-9.2c-.9 0-1.4-.5-1.4-1.3C2.9.5 3.7 0 4.6 0 5.4 0 6 .5 6 1.3c0 1-.8 1.5-1.7 1.5z" />
                 </svg>
             </button>
-            {true && (
-                // {captionClicked && (
-                <figcaption css={captionStyle}>
+            {captionClicked && (
+                <figcaption css={figCaptionStyle}>
                     <div css={iconStyle}>
-                        <svg width="36" height="23" viewBox="0 0 36 23">
-                            <path d="M3.2 0l-3.2 3.3v16.4l3.3 3.3h18.7v-23h-18.8m30.4 1l-8.6 8v5l8.6 8h2.4v-21h-2.4" />
-                        </svg>
+                        <SvgVideo size={'xsmall'} />
                     </div>
-                    {caption && (
-                        <span css={captionLink} key="caption">
-                            {caption}
-                        </span>
-                    )}
-
-                    {` ${credit}`}
+                    <div css={textStyle}>
+                        {caption && (
+                            <span css={captionStyle} key="caption">
+                                {caption}
+                            </span>
+                        )}
+                        {credit && <span css={creditStyle}>{credit}</span>}
+                    </div>
                 </figcaption>
             )}
         </>
